feffff73906c252af3d8448cc91f1253b941a576
stephengeorgewest <stephen.george.west@gmail.com>
2015-11-20 18:02:03 -0700

    Naming cleanup

--- ChromePortal/hid.js
+++ ChromePortal/hid.js
@@ -83,30 +83,44 @@ var CHARACTER_NAMES = {
 	419: "Legendary_TriggerHappy",
 	430: "Legendary_ChopChop",
 	500: "PET",
-	505: "Pet_Terrafin",
-	514: "Pet_GillGrunt",
-	519: "Pet_TriggerHappy",
-	526: "Pet_StealthElf",
-	540: "SideKick_TreeRex",
-	541: "SideKick_Thumpback",
-	542: "SideKick_Ninjini",
-	543: "SideKick_EyeBrawl"
+	505: "Pet_TerraBite",//fin",
+	514: "Pet_GillRunt",//Grunt",
+	519: "Pet_TriggerSnappy",//Happy",
+	526: "Pet_WhisperElf",//StealthElf",
+	540: "SideKick_Barkley",//TreeRex",
+	541: "SideKick_Thumpling",//back",
+	542: "SideKick_MiniJini",//Ninjini",
+	543: "SideKick_EyeSmall",//Brawl",
+	2004: "CharacterTop-Bomb",
+	1004: "CharacterBottom-Blast",
+	2010: "CharacterTop-NitroMagna",
+	1010: "CharacterBottom-Charge",
+	2015: "CharacterTop-Wash",
+	1015: "CharacterBottom-Buckler",
+	2014: "CharacterTop-Freeze",
+	1014: "CharacterBottom-Blade"
 };
 //HATS etc
 
 
 function SkylanderCharacter()
 {
-	this.serialNumber;
 	this.portalPlacementNumber;
-	this.characterId;
+
 	this.blocks = new Array();
+	this.serialNumber;
+	this.characterId;
+	this.name;
+	this.baseColor;
+	this.series;
+	this.toyType;
 	this.tradingCardId = new Array();
 	this.getTradingCardId = function()
 	{
-		var string = "";
+		var tcid = "";
 		for(var i=0; i<this.tradingCardId.length; i++)
-			string += this.tradingCardId[i] + ":";
+			tcid += this.tradingCardId[i].toString(16) + ":";
+		return tcid;
 	}
 	
 	this.getName = function()
@@ -130,9 +144,25 @@ function SkylanderCharacter()
 		{
 			
 			case 0:
-				this.serialNumber = data[0] + data[1]*0x100;
 				//serialNumber 0-2
+				this.serialNumber = data[0] + data[1]*0x100 + data[2]*0x10000//3,4also?;
+				
 				// x04 -0x0E unknown 
+				// 0x4=?
+				// 0x5 = 129
+				// 0x6 = 1 
+				// 0x7 = 15
+				// 0x8 = 196(195 spyro1)
+				// 0x9 = 56,57,39,51...
+				// 00000
+				// 0xf = 18(17 spyro1, 19 elf3 wash magna blade freeze blast)? 
+			
+				var _us = "";
+				for(var i = 0x4 ; i < array.length; i++ )
+				{
+					_us = _us + ":" + array[i].toString(16);
+				}
+				this.unknownData = _us
 				break;
 			case 1:
 				// block 1 
@@ -140,6 +170,34 @@ function SkylanderCharacter()
 				this.name = this.getName();
 				for(var i=0; i<8; i++)
 					this.tradingCardId[i] = data[4+i];
+					//0(bomb/blast,freeze/blade,wash/buckrer),
+					//2(magna/charge),
+					//5(elf3) ,0=elf, 1=legendary?, 2=elf2?, 3=dark?, 4=elf3? 
+					//0(elf,spyro1,dark),
+					//1(cynder2),
+					//0(jetvac, barkly, eyesmall)
+					//6(light core shroom boom, lc hex,lc prisim)
+				//0x0d = compatabilty/base color?
+					//32(bomb/blast,freeze/blade,wash/buckrer), 
+					//36(magna/charge),
+					//40(elf3),
+					//0(elf,spyro1,dark), 
+					//24(cynder2),
+					//16(jetvac, barkley, eyesmall)
+					//18(lightcore shroom, lc hex, lc prisim)
+				/*switch(data[0xd])
+				{
+					case 0:
+						this.base="green";
+						break;
+					case 1:
+						this.base="orange";
+						break;
+					case 3:
+					default:
+						this.base="blue";
+						break;
+				}*/
 				//0x0e, +2, CRC16 type0
 				break;
 			case 2:
@@ -166,13 +224,13 @@ function SkylanderCharacter()
 
 // portal state stuff.
 var placed_characters = new Array();
-//placed_characters[0] = {"portal_spot":[1,1], "character": new SkylanderCharacter()};
+//placed_characters[0] = {"portal_spot":1, "Character": new SkylanderCharacter()};
 var portal_spots = new Array();  
-//portal_spots[5] = {"characterNumber": 0, "character": {"name": ""}};
+//portal_spots[5] = {"placed_character": 0, "Active": true, "Stability":0...50?};
 var pending_read = new Array();//? or just use SkylanderCharacter.blocks
-// {"characterNumber: 2, "blockNumbers":[2,3,4]}
+// {"placed_character": 2, "BlockNumbers":[2,3,4]}
 var currently_reading;
-//{"characterNumber":2, "blockNumber":1, "age":4}
+//{"placed_character":2, "BlockNumber":1, "Age":4, "Retries":4}
 
 var connectionId = null;
 var usbhandle;
@@ -215,13 +273,13 @@ var myDevicePoll = function() {
 				{
 					// Convert Byte into Ascii to follow the format of our device
 					array = new Uint8Array(data);
-					string = reportID + "-" + array.length + ":" + String.fromCharCode(array[0]);
+					var raw_string = reportID + "-" + array.length + ":" + String.fromCharCode(array[0]);
 					for(var i = 1 ; i < array.length; i++ )
 					{
-						string = string + ":" + array[i];
+						raw_string = raw_string + ":" + array[i].toString(16);
 					}
 					if(myText != null)
-						myText.value = string;
+						myText.value = raw_string;
 					var message_type = String.fromCharCode(array[0]);
 					switch(message_type)
 					{
@@ -246,51 +304,58 @@ var myDevicePoll = function() {
 								{ 
 									if(portal_spots[i] == null)
 									{
-										portal_spots[i] = {"characterNumber": placed_characters.length, "character": null, "stability": 0};
-										placed_characters[placed_characters.length] = {"portal_spot": i, "character": null};
+										portal_spots[i] = {"placed_character": placed_characters.length, Active: true, "Stability": 0};
+										placed_characters[placed_characters.length] = {"portal_spot": i, "Character": null};
 									}
 									else
 									{
-										if(portal_spots[i].stability < 50)
-											portal_spots[i].stability++
-										else if(portal_spots[i].character == null)
-										{// can we get here but alread have a character populated?
-											portal_spots[i].character = new SkylanderCharacter();
-											placed_characters[portal_spots[i].characterNumber].character = portal_spots[i].character;
-											// new charcter
-											pending_read[pending_read.length] = {
-												"characterNumber":portal_spots[i].characterNumber,
-												"blockNumbers":[0,1] 
-											};
+										if(portal_spots[i].Active)
+										{
+											if(portal_spots[i].Stability < 50)
+												portal_spots[i].Stability++
+											else if(placed_characters[portal_spots[i].placed_character].Character == null)
+											{// can we get here but alread have a character populated?
+												placed_characters[portal_spots[i].placed_character].Character = new SkylanderCharacter();
+												// new charcter
+												pending_read[pending_read.length] = {
+													"placed_character": portal_spots[i].placed_character,
+													"BlockNumbers": [0,1] 
+												};
+											}
 										}
-
+										else
+										{
+											portal_spots[i].Active = true;
+										}
 									}
 
 								}
-								if(!spot_taken && portal_spots[i] != null)
+								if(!spot_taken && portal_spots[i] != null && portal_spots[i].Active)
 								{
 									// character removed
-									var num = portal_spots[i]["characterNumber"];
-									if(currently_reading != null && currently_reading.characterNumber == num)
+									var num = portal_spots[i].placed_character;
+									placed_characters[num].Character = null;
+									if(currently_reading != null && currently_reading.placed_character == num)
 										currently_reading = null;
+									// clear pending reads for the removed character
 									for(p in pending_read)
 									{
-										if(pending_read[p].characterNumber == num)
+										if(pending_read[p].placed_character == num)
 											pending_read.splice(p,1);
 									}
-									placed_characters.splice(num,1);
+									//placed_characters.splice(num,1);
 									// do we want to leave the spot blank?
 									// yeah. we do...
 									
 									// renumber if necessary...
-									for(pc in placed_characters)
+									/*for(pc in placed_characters)
 									{
 										if(pc<num)
 											continue;
 
-										portal_spots[placed_characters[pc].portal_spot].characterNumber = pc;
-									}
-									portal_spots[i] = null;
+										portal_spots[placed_characters[pc].portal_spot].placed_character = pc;
+									}*/
+									portal_spots[i].Active = false;
 								}
 							}
 							var recievedMessageCount = array[5];
@@ -304,70 +369,78 @@ var myDevicePoll = function() {
 						// remove the read from the pending reads 
 							if(array[1] < 0x10)
 							{
-								console.log("invalid characterNumber");
 								break; 
 							}
 								 
-							var characterNumber = array[1] - 0x10;
+							var placedCharacter = array[1] - 0x10;
 							var blockNumber = array[2];
 							//var name = characters[array[3] + array[4]*16];
 
 							if(currently_reading != null)
-								if(currently_reading.characterNumber == characterNumber && currently_reading.blockNumber == blockNumber)
+								if(currently_reading.placed_character == placedCharacter && currently_reading.BlockNumber == blockNumber)
 									currently_reading = null;
 									
 							var character = null;
-							if(placed_characters[characterNumber] != null)
-								character = placed_characters[characterNumber].character;
+							if(placed_characters[placedCharacter] != null)
+								character = placed_characters[placedCharacter].Character;
 								
 							if(character == null)// must be first read...
 								character = new SkylanderCharacter();
 								
-							character.importBlock(characterNumber, blockNumber, array.subarray(3, array.length));
+							character.importBlock(placedCharacter, blockNumber, array.subarray(3, array.length));
 							//var obj = {"name": name};
 							
-							placed_characters[characterNumber]["character"] = character;
-							var portal_spot = placed_characters[characterNumber]["portal_spot"];
-							portal_spots[portal_spot]["character"] = character;
+							placed_characters[placedCharacter].Character = character;
+							//var _portalSpot = placed_characters[placedCharacter]["portal_spot"];
+							//portal_spots[_portalSpot].Character = character;
 							//character type
 	   					  		 console.log(character.name);
 							break;
 
-					}
+					}// end read message
+
+
 					if(currently_reading != null && message_type != 'Z')
 					{
-						currently_reading.age ++;
-						if(currently_reading.age > 50)
+						currently_reading.Age ++;
+						if(currently_reading.Age > 50)
 						{
-							readBlock(currently_reading.characterNumber, currently_reading.blockNumber);
-							currently_reading.age = 0;
+							if(currently_reading.Retries >5)
+								currently_reading = null;
+							else
+							{
+								readBlock(currently_reading.placed_character, currently_reading.BlockNumber);
+								currently_reading.Age = 0;
+								currently_reading.Retries ++;
+							}
 						}
 					}
 					else if(pending_read.length > 0)
 					{
 						currently_reading = {
-							"characterNumber":pending_read[0].characterNumber,
-							"blockNumber": pending_read[0].blockNumbers[0],
-							"age":0
+							"placed_character": pending_read[0].placed_character,
+							"BlockNumber": pending_read[0].BlockNumbers[0],
+							"Age": 0,
+							"Retries": 0
 						};
-						readBlock(pending_read[0].characterNumber, pending_read[0].blockNumbers[0]);
-						pending_read[0].blockNumbers.splice(0,1);
-						if(pending_read[0].blockNumbers.length==0)
+						readBlock(pending_read[0].placed_character, pending_read[0].BlockNumbers[0]);
+						pending_read[0].BlockNumbers.splice(0,1);
+						if(pending_read[0].BlockNumbers.length==0)
 							pending_read.splice(0,1);
 						//read off block 1 for the character name.
-						//readBlock(portal_spots[i].characterNumber, 1, null);
+						//readBlock(portal_spots[i].placed_character, 1, null);
 					}
 
 					var characters_on_portal = "";
 					for(var i = 0; i < placed_characters.length; i++)
 						if(placed_characters[i] != null)
-							if(placed_characters[i].character != null)
-								characters_on_portal += placed_characters[i].character.name+"(" + placed_characters[i].character.serialNumber + "), ";
+							if(placed_characters[i].Character != null)
+								characters_on_portal += "#" + placed_characters[i].Character.characterId + " - " +placed_characters[i].Character.name+"(" + placed_characters[i].Character.serialNumber + "), ";
 					document.getElementById("textbox").innerText = characters_on_portal;
 
 					if(String.fromCharCode(array[0]) != 'S' && String.fromCharCode(array[0]) != 'Z')
 					{
-						console.log(string);
+						console.log(raw_string);
 					}
 				}	
 			}
